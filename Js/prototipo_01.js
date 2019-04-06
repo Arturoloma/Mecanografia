04cf1da3b4856e25bf47217eb241a5c900bc4ca5
Arturo <arturoloma3d@gmail.com>
2019-04-06 20:16:05 +0200

    Añado función EstiloInput()

--- Js/prototipo_01.js
+++ Js/prototipo_01.js
@@ -1,12 +1,13 @@
 
 // VAR GLOBALES
-var refTextoString = "En la actualidad, en lengua española, se utilizan los términos mecanógrafo y mecanógrafa para denominar a personas con conocimientos de mecanografía, es decir que, con soltura (a alta velocidad, sin necesidad de mirar el teclado) son capaces de introducir texto en una máquina de escribir. Además la mecanografía es una asignatura que por lo general suele impartirse a jóvenes que cursan la secundaria. En algunas escuelas esta disciplina incluye taquigrafía: taquimecanografía. Entre otros ejercicios que se realizan en la clase de mecanografía, se emprenden prácticas de tres quintetos, hojas enteras, etc.";
+var refTextoString = "En la actualidad"/*, en lengua española, se utilizan los términos mecanógrafo y mecanógrafa para denominar a personas con conocimientos de mecanografía, es decir que, con soltura (a alta velocidad, sin necesidad de mirar el teclado) son capaces de introducir texto en una máquina de escribir. Además la mecanografía es una asignatura que por lo general suele impartirse a jóvenes que cursan la secundaria. En algunas escuelas esta disciplina incluye taquigrafía: taquimecanografía. Entre otros ejercicios que se realizan en la clase de mecanografía, se emprenden prácticas de tres quintetos, hojas enteras, etc."*/;
 var palabras = refTextoString.match(/\S+/gi);           // Con esto evitamos obtener " xxxx" o " " como una palabra en caso de encontrar más de un espacio seguido con .split()
 var palabraActualId = 0;                                // Id de la palabra que el jugador tiene que escribir
 var refTexto = document.getElementById("ref-texto");    // Elemento del texto de referencia
 var iptTexto = document.getElementById("ipt-texto");    // Elemento del input para el usuario
 
 refTexto.innerHTML = refTextoString;                    // Asignación del texto al elemento del texto de referencia
+iptTexto.placeholder = palabras[palabraActualId];       // Placeholder de la palabra que tienes que escribir
 
 // FUNCIONES
 function GameManager(event)
@@ -22,11 +23,13 @@ function GameManager(event)
     iptTexto.value = subIpt;
 
     iptCorrecto = Comparacion(subIpt, subRef);
+    EstiloInput(iptCorrecto);
     if (iptCorrecto)
     {
       if (GameOver() === false)
       {
         AvanzarPalabra();
+        CalcularPPM();
         ResetInput();
       }
       else
@@ -40,8 +43,7 @@ function GameManager(event)
   else
   {
     iptCorrecto = Comparacion(subIpt, subRef);
-    if (iptCorrecto) { iptTexto.style.backgroundColor = "white"; }
-    else             { iptTexto.style.backgroundColor = "red";   }
+    EstiloInput(iptCorrecto);
   }
 }
 
@@ -64,5 +66,20 @@ function AvanzarPalabra()
 
 function ResetInput()
 {
+  iptTexto.placeholder = palabras[palabraActualId];
   iptTexto.value = "";
+}
+
+function EstiloInput(iptCorrecto)
+{
+  if (iptCorrecto)
+  {
+    iptTexto.style.backgroundColor = "white";
+    iptTexto.style.color = "black";
+  }
+  else
+  {
+     iptTexto.style.backgroundColor = "red";
+     iptTexto.style.color = "white";
+  }
 }
