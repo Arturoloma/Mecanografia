17339bb703ca863697f4efaa5b5612ac1df54ed3
Lemon <Blair>
2019-04-14 17:26:38 +0200

    Cronómetro
    Añadido el cronómetro al prototipo 01

--- Js/prototipo_01.js
+++ Js/prototipo_01.js
@@ -157,3 +157,35 @@ function EstiloInput(iptCorrecto)
      iptTexto.style.color = "white";
   }
 }
+
+
+/* Prototipo BR */
+var interruptortimer = true;
+
+document.getElementById('timer').innerHTML =
+  03 + ":" + 00;
+startTimer();
+
+function startTimer() {
+  if (interruptortimer == true){
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if(s==59){m=m-1}
+    if(m==0 & s==0){
+      interruptortimer = false;
+    }
+
+    document.getElementById('timer').innerHTML =
+    m + ":" + s;
+    setTimeout(startTimer, 1000);
+  }
+}
+
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
