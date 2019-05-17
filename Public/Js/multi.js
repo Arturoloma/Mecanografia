d1c36b4dd456768d6d0d78bf67e6f93c4bbf4f5d
Alberto <jalberto.dos@gmail.com>
2019-05-17 12:30:09 +0200

    Multijugador de 2pj

---
+++ Public/Js/multi.js
@@ -0,0 +1,30 @@
+"use strict"
+var socket=io.connect();
+var usuario;
+var data;
+var progress;
+var player;
+var enemigo;
+//Captura el usuario
+function usuario(){
+  usuario=document.getElementById("nombre").value;
+}
+
+//Envia su progreso al servidor
+function enviar(){
+      progress=CalcularProgreso(progreso);
+      data=[usuario,progress];
+      socket.emit("start",data);
+    }
+//Lo envia cada segundo
+setInterval(enviar,1000);
+
+
+//Recibe el porgreso al servidor y filtra el usuario que no seael mismo
+socket.on("envioProgreso",function(datos){
+  if (datos[0]!=usuario){
+    enemigo=datos[1];
+    document.getElementById("opt-progreso-multi").innerHTML=enemigo+"%";
+    document.documentElement.style.setProperty('--progreso2', enemigo + "%");
+  }
+});
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+/*
 const http = require('http');
 
 const port=process.env.PORT || 3000
@@ -18,4 +18,38 @@ server.listen(port,() => {
 
 console.log(`Server running at port `+port);
 
+});*/
+//Estancias necesarias
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var socket = require("socket.io");
+var io = socket(server);
+var port = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+var connections=[];
+var players=[];
+
+//Cuando se conecta un usuario,se guarda en connections para saber cuantos hay en total
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+//recibe la informacion de jugadores
+  socket.on("start",function(data){
+    players=data;
+    console.log("Conectados: "+players+" "+connections.length);
+  });
+});
+
+//Envia el progreso recibido a todos los usuarios
+function envioProgreso(){
+  io.sockets.emit("envioProgreso",players);
+}
+//Para que lo envie cada segundo
+setInterval(envioProgreso,1000);
+
+
+//Escucha del server
+server.listen(port, function() {
+  console.log('Servidor en el ' + port);
 });
